f69cdd761b2798e30baeaed396b1cd0acc289501
Vinothkumar <ecevin@gmail.com>
2025-01-27 11:52:37 +0530

    Add files via upload

---
+++ week2/week2day2/DropDown.java
@@ -0,0 +1,35 @@
+package Selenium_Assignments.week2.week2day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.telerik.com/contact");
+		
+		WebElement dd1=driver.findElement(By.id("Dropdown-1"));
+		Select options1=new Select(dd1);
+		options1.selectByVisibleText("General Inquiries");//Select By Visible Text
+		Thread.sleep(2000);
+		
+		WebElement dd2=driver.findElement(By.id("Dropdown-2"));
+		Select options2=new Select(dd2);
+		options2.selectByIndex(10); //Select By Index
+		Thread.sleep(2000);
+		
+		WebElement dd3=driver.findElement(By.id("Country-1"));
+		Select options3=new Select(dd3);
+		options3.selectByValue("Canada");// Select By Value
+		Thread.sleep(2000);
+		driver.close();
+		
+		
+
+	}
+
+}
---
+++ week2/week2day2/Xpath.java
@@ -0,0 +1,44 @@
+package Selenium_Assignments.week2.week2day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://login.salesforce.com/?locale=in");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+//		Method-1
+//		username
+//		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("dilip@testleaf.com");
+//		password
+//		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Test@2025");
+//		loginButton
+//		driver.findElement(By.xpath("//input[@id='Login']")).click();
+		
+		//Method-2
+		//Username
+		//WebElement username=driver.findElement(By.xpath("//input[@id='username']"));
+		WebElement username=driver.findElement(By.xpath("//input[@id='username']"));
+		username.sendKeys("dilip@testleaf.com");
+		Thread.sleep(2000);
+		
+		//Password
+		WebElement password=driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("Test@2025");
+		Thread.sleep(2000);
+		
+		//LoginButton
+		WebElement login=driver.findElement(By.xpath("//input[@id='Login']"));
+		login.click();
+		
+		driver.close();
+	}
+
+}
