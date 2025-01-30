66a7fe5e5d997d2875a482a1d7faff10fc4fdcb8
Vinothkumar <ecevin@gmail.com>
2025-01-30 13:21:01 +0530

    done

--- week2/week2day1/CreatAccount.java
+++ week2/week2day1/CreatAccount.java
@@ -1,3 +1,5 @@
+//Change the AccountName everytime while run the code to Avoid Duplicate Entry Error
+
 package Selenium_Assignments.week2.week2day1;
 
 import java.time.Duration;
@@ -8,49 +10,66 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CreatAccount {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://leaftaps.com/opentaps/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
 		WebElement username =driver.findElement(By.id("username"));
+		Thread.sleep(1000);
 		username.sendKeys("Demosalesmanager");
+		Thread.sleep(1000);
 		
 		WebElement password =driver.findElement(By.id("password"));
+		Thread.sleep(1000);
 		password.sendKeys("crmsfa");
+		Thread.sleep(1000);
 		
 		WebElement loginBtn =driver.findElement(By.className("decorativeSubmit"));
 		loginBtn.click();
-		
+		Thread.sleep(1000);
 		WebElement crmsfa =driver.findElement(By.linkText("CRM/SFA"));
+		Thread.sleep(1000);
 		crmsfa.click();
+		Thread.sleep(1000);
 
 		WebElement accounts =driver.findElement(By.linkText("Accounts"));
 		accounts.click();
+		Thread.sleep(1000);
 		
 		WebElement createac =driver.findElement(By.linkText("Create Account"));
 		createac.click();
+		Thread.sleep(1000);
 		
 		WebElement accountname =driver.findElement(By.id("accountName"));
-		accountname.sendKeys("Vinothkumar2");
+		accountname.sendKeys("Vinothkumar3");
+		Thread.sleep(1000);
 		
 		WebElement desc =driver.findElement(By.name("description"));
 		desc.sendKeys("Selenium Automation Tester");
+		Thread.sleep(1000);
 		
 		WebElement noofEmp =driver.findElement(By.id("numberEmployees"));
 		noofEmp.sendKeys("30");
+		Thread.sleep(1000);
 		
 		WebElement sname =driver.findElement(By.id("officeSiteName"));
 		sname.sendKeys("LeafTaps");
+		Thread.sleep(1000);
 		
 		WebElement createBtn =driver.findElement(By.className("smallSubmit"));
 		createBtn.click();
-				
+		Thread.sleep(1000);
+		
 		String title=driver.getTitle();
 		System.out.println(title);
+		Thread.sleep(1000);
 		
-		driver.close();
+		System.out.println("Completed Successfully");
+		Thread.sleep(1000);
+		//driver.close();
+		
 		
 	}
 
--- week2/week2day1/FacebookLogin.java
+++ week2/week2day1/FacebookLogin.java
@@ -6,19 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FacebookLogin {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
 		
 		
 		driver.findElement(By.id("email")).sendKeys("testleaf.2023@gmail.com");
+		Thread.sleep(1000);
 		
 		WebElement password=driver.findElement(By.id("pass"));
 		password.sendKeys("Tuna@321");
+		Thread.sleep(1000);
 		
 		driver.findElement(By.name("login")).click();
 		//driver.findElement(By.id("login_Layer")).click();
+		Thread.sleep(1000);
 		
 		String title=driver.getTitle();
 		System.out.println(title);
